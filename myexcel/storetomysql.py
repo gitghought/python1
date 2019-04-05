1060e5646c1a7fe4290cccc4508034c6a567b611
XEON P52 <732475885@qq.com>
2019-04-05 13:07:02 +0800

    完成了数据库类的封装，实现了数据库添加操作，测试了数据库封装类的使用步骤，共三步

--- myexcel/storetomysql.py
+++ myexcel/storetomysql.py
@@ -15,22 +15,195 @@ databaseName = "studentscore"
 #在读取数据库中的图片时，使用的地址
 mysqlPrePath = serverIP
 
+keyFiveTimesTherySname = "sname"
+keyFiveTimesTheryFiveDayTheryAvg= "fiveDayTheryAvg"
+keyFiveTimesTheryTenDayTheryAvg= "tenDayTheryAvg"
+keyFiveTimesTheryFiftenDayTheryAvg= "fiftenDayTheryAvg"
+keyFiveTimesTheryMonthTheryAvg= "monthTheryAvg"
 
+class ScoreToMySQL :
+
+    def myConnect(this):
+
+        this.db = pymysql.connect(
+            mysqlPrePath,  # 数据库对应的地址
+            username,  # 数据库登录用户名
+            password,  # 数据库登录密码
+            databaseName  # 数据库名称
+        )
+
+        return this.db
+
+    #返回一个游标
+    def getCursor(this):
+
+        #如果在此处发生异常，说明当前对象还没有调用myConnect方法
+        try:
+
+            this.cursor = this.db.cursor()
+
+        except AttributeError as e :
+
+            this.myConnect()
+
+        else :
+
+            pass
+
+        finally :
+
+            this.cursor = this.db.cursor()
+
+            return this.cursor
+    #将封装好数据的字典添加到数据库种
+    # 字典种必须封装表中需要的所有数据
+    def insertIntoFiveTimesTheryDict(this, dict):
+        try :
+            #执行向数据库插入数据的操作
+            this.cursor.execute("insert into "
+                                "fiveTimesThery ("
+                                "sname,"
+                                "fiveDayTheryAvg,"
+                                "tenDayTheryAvg,"
+                                "fiftenDayTheryAvg,"
+                                "monthTheryAvg"
+                                ") "
+                                "values("
+                                "'{sname}',"
+                                "'{fiveDayTheryAvg}',"
+                                "'{tenDayTheryAvg}',"
+                                "'{fiftenDayTheryAvg}',"
+                                "'{monthTheryAvg}'"
+                                ")".format(
+                sname=dict[keyFiveTimesTherySname],
+                fiveDayTheryAvg=dict[keyFiveTimesTheryFiveDayTheryAvg],
+                tenDayTheryAvg=dict[keyFiveTimesTheryTenDayTheryAvg],
+                fiftenDayTheryAvg=dict[keyFiveTimesTheryFiftenDayTheryAvg],
+                monthTheryAvg=dict[keyFiveTimesTheryMonthTheryAvg]
+            ))
+        except AttributeError as e:
+            this.cursor = this.getCursor()
+
+        else :
+            pass
+            this.db.commit()
+        finally:
+
+            # 执行向数据库插入数据的操作
+            this.cursor.execute("insert into "
+                                "fiveTimesThery ("
+                                "sname,"
+                                "fiveDayTheryAvg,"
+                                "tenDayTheryAvg,"
+                                "fiftenDayTheryAvg,"
+                                "monthTheryAvg"
+                                ") "
+                                "values("
+                                "'{sname}',"
+                                "'{fiveDayTheryAvg}',"
+                                "'{tenDayTheryAvg}',"
+                                "'{fiftenDayTheryAvg}',"
+                                "'{monthTheryAvg}'"
+                                ")".format(
+                sname=dict[keyFiveTimesTherySname],
+                fiveDayTheryAvg=dict[keyFiveTimesTheryFiveDayTheryAvg],
+                tenDayTheryAvg=dict[keyFiveTimesTheryTenDayTheryAvg],
+                fiftenDayTheryAvg=dict[keyFiveTimesTheryFiftenDayTheryAvg],
+                monthTheryAvg=dict[keyFiveTimesTheryMonthTheryAvg]
+            ))
+
+            this.db.commit()
+
+    #将封装好数据的字典添加到数据库种
+    #没有参数，仅用于测试功能是否可用
+    def insertIntoFiveTimesThery(this):
+        try :
+            #执行向数据库插入数据的操作
+            this.cursor.execute("insert into "
+                            "fiveTimesThery ("
+                            "sname,"
+                            "fiveDayTheryAvg,"
+                            "tenDayTheryAvg,"
+                            "fiftenDayTheryAvg,"
+                            "monthTheryAvg"
+                            ") "
+                            "values("
+                            "'{sname}',"
+                            "'{fiveDayTheryAvg}',"
+                            "'{tenDayTheryAvg}',"
+                            "'{fiftenDayTheryAvg}',"
+                            "'{monthTheryAvg}'"
+                            ")".format(
+                sname="hehehe",
+                fiveDayTheryAvg="10",
+                tenDayTheryAvg="10",
+                fiftenDayTheryAvg="10",
+                monthTheryAvg="10"
+            ))
+        except AttributeError as e:
+            this.cursor = this.getCursor()
+
+        else :
+            pass
+            this.db.commit()
+        finally:
+
+            # 执行向数据库插入数据的操作
+            this.cursor.execute("insert into "
+                                "fiveTimesThery ("
+                                "sname,"
+                                "fiveDayTheryAvg,"
+                                "tenDayTheryAvg,"
+                                "fiftenDayTheryAvg,"
+                                "monthTheryAvg"
+                                ") "
+                                "values("
+                                "'{sname}',"
+                                "'{fiveDayTheryAvg}',"
+                                "'{tenDayTheryAvg}',"
+                                "'{fiftenDayTheryAvg}',"
+                                "'{monthTheryAvg}'"
+                                ")".format(
+                sname="hehehe",
+                fiveDayTheryAvg="10",
+                tenDayTheryAvg="10",
+                fiftenDayTheryAvg="10",
+                monthTheryAvg="10"
+            ))
+
+            this.db.commit()
+
+    #执行关闭数据库的操作
+    # 在完成一个数据库操作后就执行这个函数
+    def disConnect(this):
+        this.db.close()
 
 if __name__ == '__main__':
 
-    db = pymysql.connect(
-        mysqlPrePath,  # 数据库对应的地址
-        username,  # 数据库登录用户名
-        password,  # 数据库登录密码
-        databaseName  # 数据库名称
-    )
+    dictScore = {
+        "sname":"hehehehe",
+        "fiveDayTheryAvg":"12",
+        "tenDayTheryAvg":"12",
+        "fiftenDayTheryAvg":"12",
+        "monthTheryAvg":"12"
+    }
 
-    mcursor = db.cursor()
 
-    mcursor.execute("insert into fiveTimesThery (sname) values('{sname}')".format(sname="hehehe"))
+    """
+   
+    以下是数据库使用的步骤 
+        1、创建数据库对象
+        2、添加数据库到数据库
+        3、关闭数据库连接
+    
+    """
+    #创建数据库对象
+    mstm = ScoreToMySQL()
 
-    db.commit()
+    # 将准备的字典数据添加到数据库
+    mstm.insertIntoFiveTimesTheryDict(dictScore)
 
-    db.close()
+    # 关闭本次数据库连接
+    mstm.disConnect()
+
 
