4b5d4d2c71ff5a96a5234a816406b96f23f4a92b
XEON P52 <732475885@qq.com>
2019-04-06 12:34:02 +0800

    完成学生理论和技能5、10、15、月的均分统计

--- myexcel/PickStudent.py
+++ myexcel/PickStudent.py
@@ -1,4 +1,5 @@
 import xlrd
+from myexcel import storetomysql
 
 class PickStudent ():
     # 按照表格的名称来获取指定的表格
@@ -19,18 +20,18 @@ class PickStudent ():
     # 末班学生次数统计数的列号
     degradeStudentCountColumnNumber = nameColumnNumber - 1
 
-    def __init__(self):
-        pass
+    def __init__(this, path):
+        this.path = path
 
     #根据文件路径打开Excel表格文件
-    def myXlsOpen(this, path):
-        this.workbook = xlrd.open_workbook(path)
+    def myXlsOpen(this):
+        this.workbook = xlrd.open_workbook(this.path)
         return this.workbook
 
     #打开Excel表格文件中的指定表格
     # 返回指定表格对象
-    def getSheetByName(this, sheetName):
-        this.sheet = this.workbook.sheet_by_name(sheetName)
+    def getSheetByName(this):
+        this.sheet = this.workbook.sheet_by_name(this.scoreSheetName)
         return this.sheet
 
     # 从已经打开的表格种获取指定行的所有列的值
@@ -49,12 +50,13 @@ class PickStudent ():
     # 返回与数据库对应的元祖
     # 键：sname
     # 值：学生姓名
-    def getStudentNameTuple(this, line):
+    def getStudentNameTuple(this):
+        line = this.rowValueList
         return ("sname", line[this.nameColumnNumber])
 
     # 返回指定学生名的末班次数
-    def getStudentDegradeCount(this):
-        return ("degradeCount", this.rowValueList[this.nameColumnNumber])
+    def getStudentDegradeCountTuple(this):
+        return ("degradeCount", this.rowValueList[this.degradeStudentCountColumnNumber])
 
     # 根据指定成绩单，求指定天数的平均成绩
     # 既可以求理论平均成绩，又可以求技能平均成绩
@@ -71,17 +73,21 @@ class PickStudent ():
         except TypeError as e:
             pass
 
+        print("sum = " + str(sum))
+
         return sum / nday
 
     # 获取完一行数据后，使用切片，将学生每天的成绩取出来
     #   数据包含日考、周考和月考
-    def getEveryDayScoreFromLine(this, line):
+    def getEveryDayScoreFromLine(this):
+        line = this.rowValueList
         this.scoreEveryDay = line[this.nameColumnNumber + 1:len(line) - 1]
 
         return this.scoreEveryDay
 
     # 从每天的成绩中筛选出每天的理论成绩
     def getEveryDayTheryScore(this):
+        this.getEveryDayScoreFromLine()
         this.scoreEveryDayThery = this.scoreEveryDay[::2]
         # print(scoreEveryDayThery)
 
@@ -89,65 +95,90 @@ class PickStudent ():
 
     # 从每天的成绩中筛选出每天的技能成绩
     def getEveryDaySkillScore(this):
+        this.getEveryDayScoreFromLine()
         this.scoreEveryDaySkill = this.scoreEveryDay[1::2]
 
         return this.scoreEveryDaySkill
 
     # 能够求理论5日的均分
     def getFiveDayTheryAverageScore(this):
-        return this.__getAverageScoreByDays(this.scoreEveryDayThery, 5)
+        this.getEveryDayTheryScore()
+        fiveDayTheryAverageScore = this.__getAverageScoreByDays(this.scoreEveryDayThery, 5)
+
+        return fiveDayTheryAverageScore
+
+    # 能够求理论5日的均分
+    # 返回元组数据
+    def getFiveDayTheryAverageScoreTuple(this):
+        return (storetomysql.keyFiveTimesTheryFiveDayTheryAvg, this.getFiveDayTheryAverageScore())
 
     # 能够求技能5日的均分
     def getFiveDaySkillAverageScore(this):
+        this.getEveryDaySkillScore()
+        print(this.scoreEveryDaySkill)
         return this.__getAverageScoreByDays(this.scoreEveryDaySkill, 5)
+
+    # 能够求技能5日的均分
+    def getFiveDaySkillAverageScoreTuple(this):
+        return (storetomysql.keyFiveTimesSkillFiveDaySkillAvg, this.getFiveDaySkillAverageScore())
 
     # 能够求理论10日的均分
     def getTenDayTheryAverageScore(this):
+        this.getEveryDayTheryScore()
         return this.__getAverageScoreByDays(this.scoreEveryDayThery, 10)
+        # 能够求理论10日的均分
+
+    def getTenDayTheryAverageScoreTuple(this):
+        return (storetomysql.keyFiveTimesTheryTenDayTheryAvg, this.getTenDayTheryAverageScore())
 
     # 能够求技能10日的均分
     def getTenDaySkillAverageScore(this):
+        this.getEveryDaySkillScore()
         return this.__getAverageScoreByDays(this.scoreEveryDaySkill, 10)
+
+    # 能够求技能10日的均分
+    def getTenDaySkillAverageScoreTuple(this):
+        return (storetomysql.keyFiveTimesSkillTenDaySkillAvg, this.getTenDaySkillAverageScore())
 
     # 能够求理论15日的均分
     def getFifteenDayTheryAverageScore(this):
+        this.getEveryDayTheryScore()
         return this.__getAverageScoreByDays(this.scoreEveryDayThery, 15)
+
+    # 能够求理论15日的均分
+    def getFifteenDayTheryAverageScoreTuple(this):
+        return (storetomysql.keyFiveTimesTheryFiftenDayTheryAvg, this.getFifteenDayTheryAverageScore())
 
     # 能够求技能15日的均分
     def getFifteenDaySkillAverageScore(this):
+        this.getEveryDaySkillScore()
         return this.__getAverageScoreByDays(this.scoreEveryDaySkill, 15)
 
+    # 能够求技能15日的均分
+    def getFifteenDaySkillAverageScoreTuple(this):
+        return (storetomysql.keyFiveTimesSkillFifteenDaySkillAvg, this.getFifteenDaySkillAverageScore())
 
     # 能够求理论月的均分
     def getMonthTheryAverageScore(this):
-        try :
-            monthTheryAvgScore = this.__getAverageScoreByDays( \
-                this.scoreEveryDayThery, len(this.scoreEveryDayThery))
-        except AttributeError as e :
-            this.getEveryDayTheryScore()
-        else :
-            return monthTheryAvgScore
-        finally  :
-            monthTheryAvgScore = this.__getAverageScoreByDays( \
-                this.scoreEveryDayThery, len(this.scoreEveryDayThery))
+        this.getEveryDayTheryScore()
+        monthTheryAvgScore = this.__getAverageScoreByDays( \
+            this.scoreEveryDayThery, len(this.scoreEveryDayThery))
 
-            return monthTheryAvgScore
+        return monthTheryAvgScore
+
+    # 能够求理论月的均分
+    def getMonthTheryAverageScoreTuple(this):
+        return (storetomysql.keyFiveTimesTheryMonthTheryAvg, this.getMonthTheryAverageScore())
 
     # 能够求技能月的均分
     def getMonthSkillAverageScore(this):
-        try :
-            monthSkillAvgScore = this.__getAverageScoreByDays( \
-                this.scoreEveryDaySkill, len(this.scoreEveryDaySkill))
-        except AttributeError as e :
-            this.getEveryDaySkillScore()
-        else :
-            return monthSkillAvgScore
-        finally :
-            monthSkillAvgScore = this.__getAverageScoreByDays( \
-                this.scoreEveryDaySkill, len(this.scoreEveryDaySkill))
+        this.getEveryDaySkillScore()
+        monthSkillAvgScore = this.__getAverageScoreByDays( \
+            this.scoreEveryDaySkill, len(this.scoreEveryDaySkill))
+        return monthSkillAvgScore
 
-            return monthSkillAvgScore
-
+    def getMonthSkillAverageScoreTuple(this):
+        return (storetomysql.keyFiveTimesSkillMonthSkillAvg, this.getMonthSkillAverageScore())
 
     # 求理论和技能的满月平均成绩
     def getAverageScore(this):
