a36640aba20de7e63e4a06e9edf5c61a5d760c42
XEON P52 <732475885@qq.com>
2019-04-07 20:28:36 +0800

    修复因为嵌套包含其他包导致程序执行出错的bug

--- myexcel/MyPickStudent.py
+++ myexcel/MyPickStudent.py
@@ -1,9 +1,10 @@
+from myexcel.MyInclude import MyInclude
 from myexcel.PickStudent import PickStudent
+from myexcel.storetomysql import ScoreToMySQL
 import xlwt
 import xlrd
-from myexcel.storetomysql import ScoreToMySQL
 
-scoreExcelSrc = r"./xwx_ria_1702b.xls"
+scoreExcelSrc = r"./{mclass}.xls".format(mclass = MyInclude.scoreExcelPrefix)
 
 if __name__ == '__main__':
 
@@ -15,94 +16,86 @@ if __name__ == '__main__':
     for i in range(mps.studentScoreStartPos, mps.studentScoreStopPos):
         rowValues = mps.getRowValues(i)
 
-        # print(mps.getStudentDegradeCountTuple())
-
         if mps.isNoneLine(rowValues) :
             pass
         else :
             pass
 
-            sname = mps.getStudentNameTuple()
-            print("sname = {sname}".format(sname=sname))
-
-            # fiveDayAvgScore = mps.getFiveDayTheryAverageScore()
+            # 将技能均分数据保存到数据库中
+            # sname = mps.getStudentNameTuple()
+            # print("sname = {sname}".format(sname=sname))
+            #
+            # listSkillAvgScore = []
+            # dictSkillAvgScore = {}
+            #
+            # fiveDayAvgScore = mps.getFiveDaySkillAverageScoreTuple()
             # print("fiveDayAvgScore = {fiveDayAvgScore}".format(fiveDayAvgScore=fiveDayAvgScore))
             #
-            # tenDayAvgScore = mps.getTenDayTheryAverageScore()
+            # tenDayAvgScore = mps.getTenDaySkillAverageScoreTuple()
             # print("tenDayAvgScore = {tenDayAvgScore}".format(tenDayAvgScore=tenDayAvgScore))
             #
-            # fifteenDayAvgScore = mps.getFifteenDayTheryAverageScore()
+            # fifteenDayAvgScore = mps.getFifteenDaySkillAverageScoreTuple()
             # print("fifteenDayAvgScore = {fifteenDayAvgScore}".format(fifteenDayAvgScore=fifteenDayAvgScore))
             #
-            # monthAvgScore = mps.getMonthTheryAverageScore()
-            # print("monthAvgScore = {monthAvgScore}".format(monthAvgScore=monthAvgScore))
-
-            listSkillAvgScore = []
-            dictSkillAvgScore = {}
-
-            fiveDayAvgScore = mps.getFiveDaySkillAverageScoreTuple()
-            print("fiveDayAvgScore = {fiveDayAvgScore}".format(fiveDayAvgScore=fiveDayAvgScore))
-
-            tenDayAvgScore = mps.getTenDaySkillAverageScoreTuple()
-            print("tenDayAvgScore = {tenDayAvgScore}".format(tenDayAvgScore=tenDayAvgScore))
-
-            fifteenDayAvgScore = mps.getFifteenDaySkillAverageScoreTuple()
-            print("fifteenDayAvgScore = {fifteenDayAvgScore}".format(fifteenDayAvgScore=fifteenDayAvgScore))
-
-            monthAvgScore = mps.getMonthSkillAverageScoreTuple()
-            print("monthAvgScore = {monthAvgScore}".format(monthAvgScore=monthAvgScore))
-
-
-            listSkillAvgScore.append(sname)
-            listSkillAvgScore.append(fiveDayAvgScore)
-            listSkillAvgScore.append(tenDayAvgScore)
-            listSkillAvgScore.append(fifteenDayAvgScore)
-            listSkillAvgScore.append(monthAvgScore)
-
-
-            dictSkillAvgScore = dict(listSkillAvgScore)
-
-            print(dictSkillAvgScore)
-
-            mstm = ScoreToMySQL()
-
-            # 将准备的字典数据添加到数据库
-            mstm.insertIntoFiveTimesSkillDict(dictSkillAvgScore)
-
-            # 关闭本次数据库连接
-            mstm.disConnect()
-
-            # listTheryAvgScore = []
-            # dictTheryAvgScore = {}
-            #
-            # fiveDayAvgScore = mps.getFiveDayTheryAverageScoreTuple()
-            # print("fiveDayAvgScore = {fiveDayAvgScore}".format(fiveDayAvgScore=fiveDayAvgScore))
-            #
-            # tenDayAvgScore = mps.getTenDayTheryAverageScoreTuple()
-            # print("tenDayAvgScore = {tenDayAvgScore}".format(tenDayAvgScore=tenDayAvgScore))
-            #
-            # fifteenDayAvgScore = mps.getFifteenDayTheryAverageScoreTuple()
-            # print("fifteenDayAvgScore = {fifteenDayAvgScore}".format(fifteenDayAvgScore=fifteenDayAvgScore))
-            #
-            # monthAvgScore = mps.getMonthTheryAverageScoreTuple()
+            # monthAvgScore = mps.getMonthSkillAverageScoreTuple()
             # print("monthAvgScore = {monthAvgScore}".format(monthAvgScore=monthAvgScore))
             #
             #
-            # listTheryAvgScore.append(sname)
-            # listTheryAvgScore.append(fiveDayAvgScore)
-            # listTheryAvgScore.append(tenDayAvgScore)
-            # listTheryAvgScore.append(fifteenDayAvgScore)
-            # listTheryAvgScore.append(monthAvgScore)
+            # listSkillAvgScore.append(sname)
+            # listSkillAvgScore.append(fiveDayAvgScore)
+            # listSkillAvgScore.append(tenDayAvgScore)
+            # listSkillAvgScore.append(fifteenDayAvgScore)
+            # listSkillAvgScore.append(monthAvgScore)
             #
             #
-            # dictTheryAvgScore = dict(listTheryAvgScore)
+            # dictSkillAvgScore = dict(listSkillAvgScore)
             #
-            # print(dictTheryAvgScore)
+            # print(dictSkillAvgScore)
             #
             # mstm = ScoreToMySQL()
             #
             # # 将准备的字典数据添加到数据库
-            # mstm.insertIntoFiveTimesTheryDict(dictTheryAvgScore)
+            # mstm.insertIntoFiveTimesSkillDict(dictSkillAvgScore)
             #
             # # 关闭本次数据库连接
             # mstm.disConnect()
+
+
+            # 将理论均分数据保存到数据库中
+            listTheryAvgScore = []
+            dictTheryAvgScore = {}
+
+            sname = mps.getStudentNameTuple()
+
+            fiveDayAvgScore = mps.getFiveDayTheryAverageScoreTuple()
+            print("fiveDayAvgScore = {fiveDayAvgScore}".format(fiveDayAvgScore=fiveDayAvgScore))
+
+            tenDayAvgScore = mps.getTenDayTheryAverageScoreTuple()
+            print("tenDayAvgScore = {tenDayAvgScore}".format(tenDayAvgScore=tenDayAvgScore))
+
+            fifteenDayAvgScore = mps.getFifteenDayTheryAverageScoreTuple()
+            print("fifteenDayAvgScore = {fifteenDayAvgScore}".format(fifteenDayAvgScore=fifteenDayAvgScore))
+
+            monthAvgScore = mps.getMonthTheryAverageScoreTuple()
+            print("monthAvgScore = {monthAvgScore}".format(monthAvgScore=monthAvgScore))
+
+            listTheryAvgScore.append(sname)
+            listTheryAvgScore.append(fiveDayAvgScore)
+            listTheryAvgScore.append(tenDayAvgScore)
+            listTheryAvgScore.append(fifteenDayAvgScore)
+            listTheryAvgScore.append(monthAvgScore)
+            listTheryAvgScore.append(ScoreToMySQL.getClassTuple(MyInclude.scoreExcelPrefix))
+
+            dictTheryAvgScore = dict(listTheryAvgScore)
+
+            print("dictTheryAvgScore = " + str(dictTheryAvgScore))
+
+            mstm = ScoreToMySQL()
+
+            # 将准备的字典数据添加到数据库
+            mstm.insertIntoFiveTimesTheryDict(dictTheryAvgScore)
+
+            # 关闭本次数据库连接
+            mstm.disConnect()
+
+
