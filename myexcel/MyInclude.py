1dde2a34a75555bda25ca03b6cca28bc2816d1bd
XEON P52 <732475885@qq.com>
2019-04-09 18:05:25 +0800

    添加对xls和xlsx文件格式的支持

--- myexcel/MyInclude.py
+++ myexcel/MyInclude.py
@@ -1,7 +1,23 @@
+import os
 
 
 class MyInclude () :
-    scoreExcelPrefix = "python1_gh"
+    #存储学生成绩的文件夹名称
+    studentScoreExcelFolder = r".\studentscoreexcel"
+
+
+    #存储学生成绩分析后的文件夹名称
+    studentScoreAnalizeExcelFolder = r"studentscoreanalize"
+
+    # 学校成绩单文件名，不包含
+    # scoreExcelPrefix = "python1_gh"
+    scoreExcelPrefix = "java6"
+
+    scoreExcelSrc = "{mclass}.xls".format(mclass=scoreExcelPrefix)
+    scoreExcelXSrc = "{mclass}.xlsx".format(mclass=scoreExcelPrefix)
+
+    scoreExcelAbsPath = os.path.abspath(os.path.join(studentScoreExcelFolder, scoreExcelSrc))
+    scoreExcelXAbsPath = os.path.abspath(os.path.join(studentScoreExcelFolder, scoreExcelXSrc))
 
     # 按照表格的名称来获取指定的表格
     scoreSheetName = u"成绩单"
